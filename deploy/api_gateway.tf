4dad6b09cb34caf8398cb3437665ed7d00951aa9
John Corrigan <jcorrigan@instructure.com>
2016-06-30 17:39:36 -0500

    add api gateway config for mml endpoint
    refs CNVS-29514
    
    Change-Id: I4e41c6a14c59343caa62b68bbe1afdaed6bb988e
    Reviewed-on: https://gerrit.instructure.com/84118
    Tested-by: Jenkins
    Reviewed-by: Simon Williams <simon@instructure.com>
    Product-Review: John Corrigan <jcorrigan@instructure.com>
    QA-Review: John Corrigan <jcorrigan@instructure.com>

--- deploy/api_gateway.tf
+++ deploy/api_gateway.tf
@@ -74,3 +74,70 @@ resource "aws_api_gateway_integration_response" "svg_integration_response" {
 PARAMS
 }
 
+# `GET /mml?tex=<string>`
+resource "aws_api_gateway_resource" "mml" {
+  rest_api_id = "${aws_api_gateway_rest_api.mathman_api_gateway.id}"
+  parent_id = "${aws_api_gateway_rest_api.mathman_api_gateway.root_resource_id}"
+  path_part = "mml"
+}
+
+resource "aws_api_gateway_method" "mml_method" {
+  rest_api_id = "${aws_api_gateway_rest_api.mathman_api_gateway.id}"
+  resource_id = "${aws_api_gateway_resource.mml.id}"
+  http_method = "GET"
+  authorization = "NONE"
+  request_parameters_in_json = <<PARAMS
+{
+  "method.request.querystring.tex": false
+}
+PARAMS
+}
+
+resource "aws_api_gateway_integration" "mml_integration" {
+  rest_api_id = "${aws_api_gateway_rest_api.mathman_api_gateway.id}"
+  resource_id = "${aws_api_gateway_resource.mml.id}"
+  http_method = "${aws_api_gateway_method.mml_method.http_method}"
+  type = "AWS"
+  uri = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.mathman_lambda.arn}/invocations"
+  # POST is required from API gatey to lambda, even though the consumer
+  # interface is a GET.
+  integration_http_method = "POST"
+  request_templates = {
+    "application/json" = <<TEMPLATE
+#set($inputRoot = $input.path('$'))
+{
+  "tex": "$util.urlEncode($input.params('tex'))"
+}
+TEMPLATE
+  }
+}
+
+resource "aws_api_gateway_method_response" "mml_200" {
+  rest_api_id = "${aws_api_gateway_rest_api.mathman_api_gateway.id}"
+  resource_id = "${aws_api_gateway_resource.mml.id}"
+  http_method = "${aws_api_gateway_method.mml_method.http_method}"
+  status_code = "200"
+  response_parameters_in_json = <<PARAMS
+{
+  "method.response.header.Content-Type": true,
+  "method.response.header.Vary": true
+}
+PARAMS
+}
+
+resource "aws_api_gateway_integration_response" "mml_integration_response" {
+  rest_api_id = "${aws_api_gateway_rest_api.mathman_api_gateway.id}"
+  resource_id = "${aws_api_gateway_resource.mml.id}"
+  http_method = "${aws_api_gateway_method.mml_method.http_method}"
+  status_code = "${aws_api_gateway_method_response.mml_200.status_code}"
+  response_templates = {
+    "image/mathml+xml" = "$util.parseJson($input.json('$.mml'))"
+  }
+  response_parameters_in_json = <<PARAMS
+{
+  "method.response.header.Content-Type": "'image/mathml+xml'",
+  "method.response.header.Vary": "'Accept-Encoding'"
+}
+PARAMS
+}
+
